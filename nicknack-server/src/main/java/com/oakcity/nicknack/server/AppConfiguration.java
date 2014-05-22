3d656a1b374d5a51a2c9f9f86b80c80306b90ee3
Kevin Bulebush <kmbulebu@gmail.com>
2014-05-21 22:56:04 -0400

    Stubbed out Spring server application.

---
+++ nicknack-server/src/main/java/com/oakcity/nicknack/server/AppConfiguration.java
@@ -0,0 +1,10 @@
+package com.oakcity.nicknack.server;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan
+@Configuration
+public class AppConfiguration {
+
+}
---
+++ nicknack-server/src/main/java/com/oakcity/nicknack/server/AppInitializer.java
@@ -0,0 +1,30 @@
+package com.oakcity.nicknack.server;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class AppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        WebApplicationContext context = getContext();
+        servletContext.addListener(new ContextLoaderListener(context));
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/api*");
+    }
+
+    private AnnotationConfigWebApplicationContext getContext() {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.setConfigLocation("com.oakcity.nicknack.server");
+        return context;
+    }
+
+}
