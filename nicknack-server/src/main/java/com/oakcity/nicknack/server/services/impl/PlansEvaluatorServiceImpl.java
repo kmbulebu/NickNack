84c19ae78a8c6871d33f9821a8784bb5916bda9d
Kevin Bulebush <kmbulebu@gmail.com>
2014-08-09 13:17:43 -0400

    Added variable substitution for Action parameter values.
    Variables are wrapped by double curly brackets. {{attribute name}}. For
    example, if triggering on a DSC Zone Open event, specifying {{Zone
    Label}} will insert the label of the zone that was opened into the
    parameter value.

--- nicknack-server/src/main/java/com/oakcity/nicknack/server/services/impl/PlansEvaluatorServiceImpl.java
+++ nicknack-server/src/main/java/com/oakcity/nicknack/server/services/impl/PlansEvaluatorServiceImpl.java
@@ -1,12 +1,16 @@
 package com.oakcity.nicknack.server.services.impl;
 
 import java.text.ParseException;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+import org.apache.commons.lang3.text.StrSubstitutor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,11 +23,13 @@ import rx.functions.Action1;
 import com.oakcity.nicknack.core.actions.Action;
 import com.oakcity.nicknack.core.actions.ActionFailureException;
 import com.oakcity.nicknack.core.actions.ActionParameterException;
+import com.oakcity.nicknack.core.events.AttributeDefinition;
 import com.oakcity.nicknack.core.events.Event;
 import com.oakcity.nicknack.core.events.filters.EventFilter;
 import com.oakcity.nicknack.core.events.filters.EventFilterEvaluator;
 import com.oakcity.nicknack.core.providers.ProviderService;
 import com.oakcity.nicknack.server.AppConfiguration;
+import com.oakcity.nicknack.server.model.ActionResource;
 import com.oakcity.nicknack.server.model.EventFilterResource;
 import com.oakcity.nicknack.server.model.PlanResource;
 import com.oakcity.nicknack.server.services.ActionsService;
@@ -128,7 +134,7 @@ public class PlansEvaluatorServiceImpl implements Action1<Event>{
 			for (Action action : actionsService.getActions(plan.getUUID())) {
 				LOG.info("Performing action " + action);
 				try {
-					providerService.run(action);
+					providerService.run(processVariables(action, event));
 				} catch (ActionFailureException | ActionParameterException e) {
 					LOG.warn("Failed to perform action. " + e.getMessage() + " " + action);
 					// TODO Generate and fire an Event for this error.
@@ -141,5 +147,35 @@ public class PlansEvaluatorServiceImpl implements Action1<Event>{
 		}
 
 	}
+	
+	protected Action processVariables(Action origAction, Event cause) {
+		final ActionResource newAction = new ActionResource();
+		final Map<UUID, String> newParameters = new HashMap<>();
+		newAction.setAppliesToActionDefinition(origAction.getAppliesToActionDefinition());
+		
+		for (UUID key : origAction.getParameters().keySet()) {
+			newParameters.put(key,  processVariables(origAction.getParameters().get(key), cause));
+		}
+		newAction.setParameters(newParameters);
+		return newAction;
+	}
+	
+	protected String processVariables(String value, Event cause) {
+		// Build value map
+		final Map<String, String> valueMap = new HashMap<>();
+		final Map<UUID, AttributeDefinition> attributeMap = new HashMap<>();
+		for (AttributeDefinition def : cause.getEventDefinition().getAttributeDefinitions()) {
+			attributeMap.put(def.getUUID(), def);
+		}
+		for (UUID attributeUuid : cause.getAttributes().keySet()) {
+			AttributeDefinition attribute = attributeMap.get(attributeUuid);
+			valueMap.put(attribute.getName(), cause.getAttributes().get(attributeUuid));
+		}
+		final StrSubstitutor substitutor = new StrSubstitutor(valueMap, "{{", "}}");
+		
+		return substitutor.replace(value);
+	}
+	
+	
 
 }
